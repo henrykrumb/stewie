abb929d510373d2a5582e13f21afa5e0221aeed6
Henry J. Krumb <henry.krumb@computerwerk.org>
2018-12-04 10:16:09 +0100

    update readme

--- stewie/widgettree.py
+++ stewie/widgettree.py
@@ -14,17 +14,31 @@ name2widget = {
 }
 
 
-def walk_children(node, nodewidget):
+def _walk_children(node, nodewidget):
     children = node.get('children', [])
     for child in children:
-        options = {k: child[k] for k in child if k not in ('children', 'type')}
-        childwidget = name2widget.get(child['type'])(**options)
-        walk_children(child, childwidget)
+        options = {
+            k: child[k] for k in child if k not in ('children', 'type')
+        }
+        try:
+            childtype = child['type']
+        except Exception:
+            raise RuntimeError('child widget type has to be defined')
+        childwidget = name2widget.get(childtype)(**options)
+        _walk_children(child, childwidget)
         nodewidget.add_child(childwidget)
 
 
 def build_widget_tree(parent: Container, widgets: dict):
+    """
+    :param parent: the tree is added to the parent Container
+    :param widgets: dictionary representing the widget tree
+    """
     root = widgets
+    try:
+        roottype = root['type']
+    except:
+        raise RuntimeError('root widget type has to be defined')
     rootwidget = name2widget.get(root['type'])()
-    walk_children(root, rootwidget)
+    _walk_children(root, rootwidget)
     parent.add_child(rootwidget)
