e725b18d5373c800514369cba4beb392b3f62688
Henry J. Krumb <henry.krumb@computerwerk.org>
2018-12-09 17:33:39 +0100

    select demo from cmd arguments in run.py

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,8 @@
 # CHANGELOG
 
+# 1.2
+
 # 1.1
+* project has a demos/ directory
+* ListBox scrolling implemented
+* integration of UNIX signal handlers
--- demos/main.py
+++ demos/main.py
@@ -4,5 +4,17 @@ from .listbox import demo_listbox
 from .vbox import demo_vbox
 
 
+demos = {
+    'listbox': demo_listbox,
+    'vbox': demo_vbox
+}
+
+
 def main():
-    demo_listbox()
+    parser = argparse.ArgumentParser()
+    choices = list(demos.keys())
+    parser.add_argument('DEMO',
+                        choices=choices)
+    args = parser.parse_args()
+    demo = args.DEMO
+    demos.get(demo)()
